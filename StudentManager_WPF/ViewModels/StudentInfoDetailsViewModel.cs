717dde009db37f83ae215cc03d875372eafb40c2
Nam Nguyen <nam.nguyen19@sotatek.com>
2025-01-16 16:10:52 +0700

    mend

--- StudentManager_WPF/ViewModels/StudentInfoDetailsViewModel.cs
+++ StudentManager_WPF/ViewModels/StudentInfoDetailsViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StudentManager_WPF.ViewModels
 {
-    class StudentInfoDetailsViewModel
+    class StudentInfoDetailsViewModel : ViewModelBase
     {
     }
 }
